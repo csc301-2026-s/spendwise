d4065d2fa56c8063f38bf280e5c32522ce3ddb75
redpinecube <tara.chakkithara@icloud.com>
2026-02-22 13:35:49 -0600

    implemented login functionality
    Signed-off-by: redpinecube <tara.chakkithara@icloud.com>

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,8 +41,13 @@ INSTALLED_APPS = [
     'accounts',
     'rest_framework',
     'corsheaders',
-
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
