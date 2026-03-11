e43fa77e51fbf0779ac5de3b533588cb4207055e
Ali Hassan Shaikh <160358245+alihassan200@users.noreply.github.com>
2026-03-11 16:33:00 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,6 @@ import Scholarships from "./components/Scholarships"
 import Onboarding from "./components/Onboarding"
 import { getToken, isOnboardingComplete } from "./utils/session"
 import Plaid from "./components/Plaid"
-//Import student codes route
-import StudentCodes from "./components/StudentCodes";
-import StudentCodeDetail from "./components/StudentCodeDetail";
 import Transactions from "./components/Transactions"
 import Investing from "./components/Investing"
 
@@ -31,19 +28,22 @@ const PublicRoute = ({ children }) => {
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PublicRoute><Registration /></PublicRoute>} />
-        <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
-        <Route path="/onboarding" element={<ProtectedRoute><Onboarding /></ProtectedRoute>} />
-        <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-        <Route path="/scholarships" element={<ProtectedRoute><Scholarships /></ProtectedRoute>} />
-        <Route path="/transactions" element={<ProtectedRoute><Transactions /></ProtectedRoute>} />
-        <Route path="/investing" element={<ProtectedRoute><Investing /></ProtectedRoute>} /> 
-        <Route path="*" element={<Navigate to="/login" replace />} />
-        <Route path="/student-codes" element={<ProtectedRoute><StudentCodes /></ProtectedRoute>} />
-        <Route path="/student-codes/deal/:id" element={<ProtectedRoute><StudentCodeDetail /></ProtectedRoute>} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PublicRoute><Registration /></PublicRoute>} />
+          <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
+          <Route path="/onboarding" element={<ProtectedRoute><Onboarding /></ProtectedRoute>} />
+          <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/scholarships" element={<ProtectedRoute><Scholarships /></ProtectedRoute>} />
+          <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+          <Route path="/my-scholarships" element={<ProtectedRoute><MyScholarships /></ProtectedRoute>} />
+          <Route path="/transactions" element={<ProtectedRoute><Transactions /></ProtectedRoute>} />
+          <Route path="/investing" element={<ProtectedRoute><Investing /></ProtectedRoute>} /> 
+          <Route path="/student-codes/deal/:id" element={<ProtectedRoute><StudentCodeDetail /></ProtectedRoute>} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
